fc6e58ded5727988002178e9be5ed0e012fd6e75
NTR-TheMekaVerseclear <tamekastevenson7@gmail.com>
2022-05-18 10:51:28 -0700

    initial commit - carousel on home page

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
+import { UncontrolledCarousel } from 'reactstrap'
 
 export default class Home extends Component {
+
+
   render() {
     return (
-      <h3>Home</h3>
+      <>
+
+        <UncontrolledCarousel
+          items={[
+            {
+              altText: 'Dilapilated House 1',
+              caption: 'Slumlord Estates is your favorite property management company ',
+              key: 1,
+              src: 'https://images.unsplash.com/photo-1444419988131-046ed4e5ffd6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+            },
+            {
+              altText: 'Dilapilated House 2',
+              caption: 'List all of your wonderful dilapidated properties with us!',
+              key: 2,
+              src: 'https://images.unsplash.com/photo-1516822671976-480ad5131e92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+            }
+          ]}
+        />
+
+      </>
     )
   }
 }
